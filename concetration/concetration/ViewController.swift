84a88e8256a155d6b380e8a76b21ca3d0c9c6ad4
Carlos Arenas <carenasdev@gmail.com>
2018-08-02 01:12:05 -0500

    Setting the actions for the cards

--- concetration/concetration/ViewController.swift
+++ concetration/concetration/ViewController.swift
@@ -8,8 +8,18 @@
 
 import UIKit
 
+// Functions can go outside the class making the global. But we intend to keep everything object oriented
+
 class ViewController: UIViewController {
 
+    @IBAction func touchCards(_ sender: UIButton) {
+        flipCard(withEmoji: "😬", on: sender)
+    }
+    
+    // The requirment for writtring a function with parameters is that it should read like english
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
