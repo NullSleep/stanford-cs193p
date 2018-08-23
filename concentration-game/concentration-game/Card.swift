8876a84a1ccb4a9b8830cf2ab544d1d26cf03a9e
Carlos Arenas <carenasdev@gmail.com>
2018-08-23 17:07:54 -0500

    Adding the models Concentration and Card

---
+++ concentration-game/concentration-game/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  concentration-game
+//
+//  Created by Carlos Arenas on 8/23/18.
+//  Copyright © 2018 Polygon. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var isFaceUp = false
+    var isMatched = false
+    var identifier = Int
+}
---
+++ concentration-game/concentration-game/Concetration.swift
@@ -0,0 +1,18 @@
+//
+//  Concetration.swift
+//  concentration-game
+//
+//  Created by Carlos Arenas on 8/23/18.
+//  Copyright © 2018 Polygon. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    // MARK: - Public Application Program Interface
+    var cards: Array<Card>
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
