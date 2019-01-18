e4245958dcd7e9aae7c774723c23e3ace31e10ff
Carlos Arenas <nullsleep@me.com>
2019-01-18 02:06:16 -0500

    EmojiArt project

---
+++ EmojiArt/EmojiArt/EmojiArtViewController.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Carlos Arenas on 1/18/19.
+//  Copyright © 2019 Polygon. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
